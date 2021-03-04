0e7928a43737a9fe1a28e9306948f7fce42e5b3f
Minh Ngo <ngo.minh0224@gmail.com>
2021-03-03 22:35:53 -0800

    get, post,put

---
+++ data/todos.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    todo: 'Make bed in the morning',
+    completed: false,
+  },
+  {
+    todo: 'Attend lecture for school',
+    completed: false,
+  },
+  {
+    todo: 'Walk the dog',
+    completed: false,
+  },
+  {
+    todo: 'Vacuum carpet',
+    completed: false,
+  },
+  {
+    todo: 'Brush teeth at night',
+    completed: false,
+  },
+];
